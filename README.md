5dce3a34394ee3efab88d0f208c63a4a84d2f7f2
Grendel <jcg@grendel.world>
2022-09-07 19:26:55 -0500

    Obliterate history. New intial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# imgup-rs
+
+This is a Rust CLI tool for pushing image files up to imgur and copying the URL to the system clipboard.
+
+## Example
+Usage:
+```bash
+$ imgup ~/pictures/file.jpg
+```
+Output:
+```bash
+https://i.imgur.com/rnSZuQH.jpg
+Delete hash: <hash>
+```
+
+## Notes
+
+Currently does not support copying URL to clipboard.
+
+## TODO
+
+- [x] Enable image upload
+- [ ] Enable copying URL to clipboard
+
+although you can just copy the url to your clipboard yourself
+
+stop bein lazy
---
+++ src/main.rs
@@ -0,0 +1,73 @@
+extern crate base64;
+extern crate reqwest;
+extern crate serde;
+extern crate chrono;
+extern crate anyhow;
+
+use std::{fs, process, env, path::Path};
+use std::io::Write;
+use std::collections::HashMap;
+
+use serde::Deserialize;
+
+
+#[derive(Deserialize)]
+struct Data {
+    link: String,
+    deletehash: String
+}
+
+#[derive(Deserialize)]
+struct ImgUrl {
+    data: Data
+}
+
+
+fn main() -> Result<(), anyhow::Error>
+{
+    if env::args().count() != 2 {
+        println!("Usage: imgup <filename>");
+        process::exit(1);
+    }
+
+    let file = env::args().last().expect("No argument given");
+    let data = fs::read(&file)?;
+    let b64  = base64::encode(&data);
+
+    let mut image = HashMap::new();
+    image.insert("image", &b64);
+
+    let mut clientidpath = env::current_exe().expect("Couldn't get executable path name");
+    _ = clientidpath.pop();
+    clientidpath.push("imgup.secret");
+
+    let clientid = fs::read_to_string(clientidpath).expect("Coudln't slurp secret file");
+    let auth_value = format!("Client-ID {}", &clientid.trim());
+    let res: ImgUrl = reqwest::blocking::Client::new()
+        .post("https://api.imgur.com/3/image")
+        .header(reqwest::header::AUTHORIZATION, auth_value.as_str())
+        .form(&image)
+        .send()?
+        .json()?;
+
+    println!("{}", res.data.link);
+    eprintln!("Delete: {}", res.data.deletehash);
+
+    let logfile = match env::var("IMGUP_LOGFILE") {
+        Ok(s) => s,
+        Err(_) => env::var("HOME")? + "/.imgup_log",
+    };
+    let mut logfile = fs::OpenOptions::new().append(true).create(true).open(logfile)?;
+    writeln!(&mut logfile,
+             "{}\t{}\t{}\t{}",
+             chrono::Local::now().format("%F_%T"),
+             Path::new(&file).file_name()
+                             .expect(&format!("Can't basename {}", &file))
+                             .to_str()
+                             .expect("Can't get basename to_str()"),
+             res.data.link,
+             &format!("https://imgur.com/delete/{}", res.data.deletehash)
+             )?;
+
+    Ok(())
+}
