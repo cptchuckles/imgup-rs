e01b2d6ed112ebbf251d8aac12f42325633c7337
Grendel <jcg@grendel.world>
2022-10-17 15:49:30 -0500

    Run LSP formatter

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,27 @@
+extern crate anyhow;
 extern crate base64;
+extern crate chrono;
 extern crate reqwest;
 extern crate serde;
-extern crate chrono;
-extern crate anyhow;
 
-use std::{fs, process, env, path::Path};
-use std::io::Write;
 use std::collections::HashMap;
+use std::io::Write;
+use std::{env, fs, path::Path, process};
 
 use serde::Deserialize;
-
 
 #[derive(Deserialize)]
 struct Data {
     link: String,
-    deletehash: String
+    deletehash: String,
 }
 
 #[derive(Deserialize)]
 struct ImgUrl {
-    data: Data
+    data: Data,
 }
 
-
-fn main() -> Result<(), anyhow::Error>
-{
+fn main() -> Result<(), anyhow::Error> {
     if env::args().count() != 2 {
         println!("Usage: imgup <filename>");
         process::exit(1);
@@ -32,7 +29,7 @@ fn main() -> Result<(), anyhow::Error>
 
     let file = env::args().last().expect("No argument given");
     let data = fs::read(&file)?;
-    let b64  = base64::encode(&data);
+    let b64 = base64::encode(&data);
 
     let mut image = HashMap::new();
     image.insert("image", &b64);
@@ -50,6 +47,7 @@ fn main() -> Result<(), anyhow::Error>
     ));
 
     let auth_value = format!("Client-ID {}", &clientid.trim());
+
     let res: ImgUrl = reqwest::blocking::Client::new()
         .post("https://api.imgur.com/3/image")
         .header(reqwest::header::AUTHORIZATION, auth_value.as_str())
@@ -64,17 +62,24 @@ fn main() -> Result<(), anyhow::Error>
         Ok(s) => s,
         Err(_) => env::var("HOME")? + "/imgup.log",
     };
-    let mut logfile = fs::OpenOptions::new().append(true).create(true).open(logfile)?;
-    writeln!(&mut logfile,
-             "{}\t{}\t{}\t{}",
-             chrono::Local::now().format("%F_%T"),
-             Path::new(&file).file_name()
-                             .expect(&format!("Can't basename {}", &file))
-                             .to_str()
-                             .expect("Can't get basename to_str()"),
-             res.data.link,
-             &format!("https://imgur.com/delete/{}", res.data.deletehash)
-             )?;
+
+    let mut logfile = fs::OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(logfile)?;
+
+    writeln!(
+        &mut logfile,
+        "{}\t{}\t{}\t{}",
+        chrono::Local::now().format("%F_%T"),
+        Path::new(&file)
+            .file_name()
+            .expect(&format!("Can't basename {}", &file))
+            .to_str()
+            .expect("Can't get basename to_str()"),
+        res.data.link,
+        &format!("https://imgur.com/delete/{}", res.data.deletehash)
+    )?;
 
     Ok(())
 }
